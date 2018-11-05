5f5bfa73375e145b6007db18a22ce1f65a87ebc8
Mario Smolcic <msmolcic92@gmail.com>
2018-11-05 12:22:58 +0100

    Added two new sections.

---
+++ Tutorial/Classes/User.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Classes
+{
+    public class User
+    {
+        /// <summary>
+        /// Global variable of a class is called a "field".
+        /// It also consists of an access modifier, optional
+        /// modifier, data type and a name.
+        /// </summary>
+        private DateTime birthDate;
+
+        /// <summary>
+        /// Constructor is basically a method used to create instance
+        /// of an object of a certain class.
+        /// 
+        /// Only difference between a method and a constructor is that
+        /// constructor has no return type (since it returns an object
+        /// of a type of the class within whom it exists) and its name
+        /// must be the same as the name of the class.
+        /// 
+        /// By default, every class has an empty constructor as this one
+        /// which is not visible and does not have to be defined explicitly.
+        /// However, as soon as we introduce a different constructor for
+        /// the class, the default one disappears and has to be manually
+        /// defined if we want to have it.
+        /// </summary>
+        public User()
+        {
+        }
+
+        /// <summary>
+        /// Calling ": this()" will call the empty constructor of User class.
+        /// If User class had a parent class we would be able to call any
+        /// of the parent class constructors by using ": base(optionalParametersHere)".
+        /// 
+        /// Logic of the called constructor is always executed before the logic of the
+        /// caller constructor. For example, logic within a parent class constructor
+        /// will be completed before the logic within a child constructor when called.
+        /// </summary>
+        /// <param name="name">User's name.</param>
+        public User(string name) : this()
+        {
+            // "this" keyword is referring to the class we're in currently.
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Element of a class that encapsulates the way we get and/or set
+        /// the value of a class "field" is called a "property".
+        /// 
+        /// It contains of the same parts as a field except it also has
+        /// a "get;" and "set;" keywords as a suffix.
+        /// Those two are C# sugar syntax for the getter and setter methods.
+        /// </summary>
+        public string Name { get; set; }
+
+        /* 
+         * Property "Name" defined above this sentence is a shorthand for the code that follows:
+         * 
+         * private string name;
+         * 
+         * public string GetName()
+         * {
+         *     return this.name;
+         * }
+         * 
+         * public void SetName(string name)
+         * {
+         *     this.name = name;
+         * }
+         * 
+         * Creators of C# were probably sick of defining getters and setters for each
+         * class "field" and decided to create a class "property" to keep the class
+         * definition concise and clean while retaining the ability to encapsulate
+         * the logic of getting and setting the field value if needed.
+         * 
+         */
+
+        /// <summary>
+        /// This example of setting a value to the BirtDate property of the User class is
+        /// showing that we can use an additional C# keyword while being in a scope
+        /// of "set" method of our property, which is "value". It contains the value
+        /// someone is trying to set to the BirtDate property in this case.
+        /// </summary>
+        public DateTime BirtDate
+        {
+            get { return this.birthDate; }
+
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    Console.WriteLine("Date of birth can't be set to a future date.");
+
+                    // Since "set" method of a property is not returning any value (void).
+                    // Keyword "return;" (without any return value), will exit the logic
+                    // of setting a value when reached. That's the reason there's no
+                    // "else" case to this "if". If code flow does not enter the logic of
+                    // "if" block, it will set the new value of BirthDate successfully.
+                    return;
+                }
+
+                this.birthDate = value;
+            }
+        }
+    }
+}
---
+++ Tutorial/Methods/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Methods
+{
+    /*
+     * A method is a code block that contains a series of statements.
+     * A program causes the statements to be executed by calling the
+     * method and specifying any required method parameters. In C#,
+     * every executed instruction is performed in the context of a method.
+     */
+    public class Program
+    {
+        /// <summary>
+        /// When it comes to the Console Applications, static Main method
+        /// is always an entry point for the application.
+        /// </summary>
+        /// <param name="args">
+        /// An array of arguments passed into Console Application
+        /// when application was started.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(2, 3));
+            Console.WriteLine(Add(3, 4));
+        }
+
+        /// <summary>
+        /// Every method consists of:
+        /// - access modifier (private, protected, internal, public)
+        /// - optional modifier (abstract, sealed, virtual, static)
+        /// - return value (such as: void, int, decimal, string, object...)
+        /// - method name
+        /// - any method parameters (none is also acceptable)
+        /// </summary>
+        /// <param name="firstNumber">First number to add up.</param>
+        /// <param name="secondNumber">Second number to add up.</param>
+        /// <returns>Sum of two numbers.</returns>
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        /*
+         * Lets take a look at the following example of method:
+         * 
+         * static void Add(int firstNumber, int secondNumber)
+         * {
+         * }
+         * 
+         * Having such method within Program class would result with an error.
+         * Why? Because this Add method and the one above have the same "method signature".
+         * Method signature consists of method name and method parameters.
+         * (method parameters must differ by type, their name is irrelevant)
+         * As long as method signature is unique within a class, it can coexist with other methods.
+         *
+         * METHOD OVERLOAD:
+         * Having multiple methods with the same name and different parameters is called method overload.
+         * For example, having an Add method which accepts two "decimal" numbers instead of two "int" numbers
+         * is perfectly fine to have within this class. Different method overload would then be called depending
+         * on the type of the variables passed when calling Add method.
+         * 
+         */
+    }
+}
